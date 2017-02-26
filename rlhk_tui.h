27808c25aafd402f0bcdcb74b399228d45d391ec
Christopher Wellons <wellons@nullprogram.com>
2017-02-26 10:28:28 -0500

    Move character defines out of implementation.

--- rlhk_tui.h
+++ rlhk_tui.h
@@ -140,20 +140,7 @@ void rlhk_tui_title(const char *);
 RLHK_TUI_API
 void rlhk_tui_size(int *width, int *height);
 
-/* Implementation */
-#ifdef RLHK_TUI_IMPLEMENTATION
-
-static int rlhk_tui_width;
-static int rlhk_tui_height;
-
 #if (defined(__unix__) || defined(__APPLE__)) && !defined(__DJGPP__)
-/* Here we're free to assume an int is 32-bits. */
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <termios.h>
-#include <sys/ioctl.h>
-
 #define RLHK_TUI_SPACE                                              0x000020U
 #define RLHK_TUI_EXCLAMATION_MARK                                   0x000021U
 #define RLHK_TUI_QUOTATION_MARK                                     0x000022U
@@ -376,174 +363,7 @@ static int rlhk_tui_height;
 #define RLHK_TUI_SUPERSCRIPT_LATIN_SMALL_LETTER_N                   0xbf81e2U
 #define RLHK_TUI_SUPERSCRIPT_TWO                                    0x00b2c2U
 #define RLHK_TUI_BLACK_SQUARE                                       0xa096e2U
-
-struct termios rlhk_tui_termios_orig;
-
-/* The last written display. */
-static unsigned rlhk_tui_oldc[RLHK_TUI_MAX_HEIGHT][RLHK_TUI_MAX_WIDTH];
-static unsigned char rlhk_tui_olda[RLHK_TUI_MAX_HEIGHT][RLHK_TUI_MAX_WIDTH];
-
-/* Characters to be written on the next flush. */
-static unsigned rlhk_tui_bufc[RLHK_TUI_MAX_HEIGHT][RLHK_TUI_MAX_WIDTH];
-static unsigned char rlhk_tui_bufa[RLHK_TUI_MAX_HEIGHT][RLHK_TUI_MAX_WIDTH];
-
-static unsigned char *
-rlhk_tui_itoa(unsigned char *p, int v)
-{
-    int len = !v;
-    int i;
-    for (i = v; i; i /= 10)
-        len++;
-    for (i = len - 1; i >= 0; i--, v /= 10)
-        p[i] = v % 10 + '0';
-    return p + len;
-}
-
-RLHK_TUI_API
-int
-rlhk_tui_init(int width, int height)
-{
-    struct termios raw;
-    char init[] = {
-        "\x1b[2J"   /* Clear the screen. */
-        "\x1b[?25l" /* Hide the cursor. */
-    };
-    rlhk_tui_width = width;
-    rlhk_tui_height = height;
-    tcgetattr(STDIN_FILENO, &rlhk_tui_termios_orig);
-    memcpy(&raw, &rlhk_tui_termios_orig, sizeof(raw));
-    raw.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
-    raw.c_oflag &= ~OPOST;
-    raw.c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN);
-    raw.c_cflag &= ~(CSIZE|PARENB);
-    raw.c_cflag |= CS8;
-    tcsetattr(STDIN_FILENO, TCSANOW, &raw);
-    return write(STDIN_FILENO, init, sizeof(init) - 1) == sizeof(init) - 1;
-}
-
-RLHK_TUI_API
-void
-rlhk_tui_release(void)
-{
-    char finish[24] =
-        "\x1b[?25h"  /* Restore cursor visibility. */
-        "\x1b[";     /* Move cursor just outside drawing region. */
-    unsigned char *f = (unsigned char *)finish + strlen(finish);
-    char *p = (char *)rlhk_tui_itoa(f, rlhk_tui_height);
-    strcpy(p, ";0H" "\x1b[0m\n");  /* Disable color/style. */
-    tcsetattr(STDIN_FILENO, TCSANOW, &rlhk_tui_termios_orig);
-    write(STDIN_FILENO, finish, sizeof(finish) - 1);
-    memset(rlhk_tui_oldc, 0, sizeof(rlhk_tui_oldc));
-}
-
-RLHK_TUI_API
-void
-rlhk_tui_putc(int x, int y, unsigned c, unsigned attr)
-{
-    rlhk_tui_bufc[y][x] = c;
-    rlhk_tui_bufa[y][x] = attr;
-}
-
-RLHK_TUI_API
-void
-rlhk_tui_flush(void)
-{
-    int x, y;
-    /* Estimated worst-case buffer. */
-    static unsigned char buf[RLHK_TUI_MAX_HEIGHT * RLHK_TUI_MAX_WIDTH *
-                             (5 + 11 + 3)];
-    unsigned char *p = buf;
-    unsigned last_a = -1u;
-    int cx = -1;
-    int cy = -1;
-    for (y = 0; y < rlhk_tui_height; y++) {
-        for (x = 0; x < rlhk_tui_width; x++) {
-            unsigned *oc = &rlhk_tui_oldc[y][x];
-            unsigned char *oa = &rlhk_tui_olda[y][x];
-            unsigned c = rlhk_tui_bufc[y][x];
-            unsigned a = rlhk_tui_bufa[y][x];
-            if (*oc != c || *oa != a) {
-                /* Move to location. */
-                if (x != cx || y != cy) {
-                    *p++ = 0x1b;
-                    *p++ = '[';
-                    p = rlhk_tui_itoa(p, y + 1);
-                    *p++ = ';';
-                    p = rlhk_tui_itoa(p, x + 1);
-                    *p++ = 'H';
-                    last_a = -1u;
-                }
-                /* Apply colors. */
-                if (a != last_a) {
-                    int fg = (a & 0x07) + 30;
-                    int bg = ((a >> 4) & 0x07) + (a & 0x80 ? 100 : 40);
-                    *p++ = 0x1b;
-                    *p++ = '[';
-                    if (a & 0x08) {
-                        *p++ = '1';
-                        *p++ = ';';
-                    }
-                    p = rlhk_tui_itoa(p, fg);
-                    *p++ = ';';
-                    p = rlhk_tui_itoa(p, bg);
-                    *p++ = 'm';
-                    last_a = a;
-                }
-                cx = x + 1;
-                cy = y;
-                *oc = c;
-                *oa = a;
-                do {
-                    *p++ = c;
-                    c >>= 8;
-                } while (c);
-            }
-        }
-    }
-    write(STDOUT_FILENO, buf, p - buf);
-}
-
-RLHK_TUI_API
-int
-rlhk_tui_getch(void)
-{
-    int r;
-    unsigned char c;
-    if ((r = read(STDIN_FILENO, &c, sizeof(c))) < 0) {
-        return r;
-    } else if (c == '\x1b') {
-        unsigned char code[2];
-        (void)read(STDIN_FILENO, code, sizeof(code));
-        return code[1] + 256;
-    } else {
-        return c;
-    }
-}
-
-RLHK_TUI_API
-void
-rlhk_tui_title(const char *title)
-{
-    char a = '\a';
-    write(STDIN_FILENO, "\x1b]2;", 4);
-    write(STDIN_FILENO, title, strlen(title));
-    write(STDIN_FILENO, &a, 1);
-}
-
-RLHK_TUI_API
-void
-rlhk_tui_size(int *width, int *height)
-{
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    *width = w.ws_col;
-    *height = w.ws_row;
-}
-
 #elif defined(_WIN32)
-#include <windows.h>
-#include <conio.h>
-
 #define RLHK_TUI_SPACE                                              0x0020
 #define RLHK_TUI_EXCLAMATION_MARK                                   0x0021
 #define RLHK_TUI_QUOTATION_MARK                                     0x0022
@@ -766,137 +586,7 @@ rlhk_tui_size(int *width, int *height)
 #define RLHK_TUI_SUPERSCRIPT_LATIN_SMALL_LETTER_N                   0x207f
 #define RLHK_TUI_SUPERSCRIPT_TWO                                    0x00b2
 #define RLHK_TUI_BLACK_SQUARE                                       0x25a0
-
-static CHAR_INFO rlhk_tui_buf[RLHK_TUI_MAX_HEIGHT * RLHK_TUI_MAX_WIDTH];
-static HANDLE rlhk_tui_out;
-static HANDLE rlhk_tui_in;
-static DWORD rlhk_tui_mode_orig;
-
-RLHK_TUI_API
-int
-rlhk_tui_init(int width, int height)
-{
-    CONSOLE_CURSOR_INFO info = {100, FALSE};
-    SMALL_RECT window = {0, 0, 0, 0};
-    COORD size;
-    DWORD mode;
-    rlhk_tui_width = width;
-    rlhk_tui_height = height;
-    rlhk_tui_out = GetStdHandle(STD_OUTPUT_HANDLE);
-    rlhk_tui_in  = GetStdHandle(STD_INPUT_HANDLE);
-
-    /* Adjust console input and cursor. */
-    SetConsoleCursorInfo(rlhk_tui_out, &info);
-    GetConsoleMode(rlhk_tui_in, &rlhk_tui_mode_orig);
-    mode = rlhk_tui_mode_orig & ~ENABLE_PROCESSED_INPUT;
-    SetConsoleMode(rlhk_tui_in, mode);
-
-    /* Adjust window size. */
-    window.Bottom = height - 1;
-    window.Right = width - 1;
-    if (!SetConsoleWindowInfo(rlhk_tui_out, TRUE, &window))
-        return 0;
-
-    /* Adjust buffer size. */
-    size.X = width;
-    size.Y = height;
-    if (!SetConsoleScreenBufferSize(rlhk_tui_out, size))
-        return 0;
-
-    return 1;
-}
-
-RLHK_TUI_API
-void
-rlhk_tui_release(void)
-{
-    CONSOLE_CURSOR_INFO info = {100, TRUE};
-    COORD coord = {0, 0};
-    coord.Y = rlhk_tui_height;
-    SetConsoleCursorInfo(rlhk_tui_out, &info);
-    SetConsoleCursorPosition(rlhk_tui_out, coord);
-    SetConsoleMode(rlhk_tui_in, rlhk_tui_mode_orig);
-}
-
-RLHK_TUI_API
-void
-rlhk_tui_putc(int x, int y, unsigned c, unsigned attr)
-{
-    rlhk_tui_buf[y * rlhk_tui_width + x].Char.UnicodeChar = c;
-    rlhk_tui_buf[y * rlhk_tui_width + x].Attributes =
-        (attr & 0xaa) | ((attr >> 2) & 0x11) | ((attr << 2) & 0x44);
-}
-
-RLHK_TUI_API
-void
-rlhk_tui_flush(void)
-{
-    COORD origin = {0, 0};
-    SMALL_RECT area = {0, 0, 0, 0};
-    COORD size;
-    area.Right = rlhk_tui_width;
-    area.Bottom = rlhk_tui_height;
-    size.X = rlhk_tui_width;
-    size.Y = rlhk_tui_height;
-    WriteConsoleOutputW(rlhk_tui_out, rlhk_tui_buf, size, origin, &area);
-}
-
-RLHK_TUI_API
-int
-rlhk_tui_getch(void)
-{
-    int result = getch();
-    if (result != 0xE0 && result != 0x00) {
-        return result;
-    } else {
-        result = getch();
-        switch (result) {
-            case 72:
-                return RLHK_TUI_VK_U;
-            case 80:
-                return RLHK_TUI_VK_D;
-            case 75:
-                return RLHK_TUI_VK_L;
-            case 77:
-                return RLHK_TUI_VK_R;
-            case 71:
-                return RLHK_TUI_VK_UL;
-            case 73:
-                return RLHK_TUI_VK_UR;
-            case 79:
-                return RLHK_TUI_VK_DL;
-            case 81:
-                return RLHK_TUI_VK_DR;
-            default:
-                return result + 256;
-        }
-    }
-}
-
-RLHK_TUI_API
-void
-rlhk_tui_title(const char *title)
-{
-    SetConsoleTitle(title);
-}
-
-RLHK_TUI_API
-void
-rlhk_tui_size(int *width, int *height)
-{
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    *width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-    *height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-}
-
-#elif defined(__DJGPP__)
-#ifdef __STRICT_ANSI__
-#  undef __STRICT_ANSI__  /* Disable DJGPP annoyance. */
-#endif
-#include <conio.h>
-#include <sys/nearptr.h>
-
+#elif defined(__MSDOS__)
 #define RLHK_TUI_SPACE                                              0x20
 #define RLHK_TUI_EXCLAMATION_MARK                                   0x21
 #define RLHK_TUI_QUOTATION_MARK                                     0x22
@@ -1119,6 +809,318 @@ rlhk_tui_size(int *width, int *height)
 #define RLHK_TUI_SUPERSCRIPT_LATIN_SMALL_LETTER_N                   0xfc
 #define RLHK_TUI_SUPERSCRIPT_TWO                                    0xfd
 #define RLHK_TUI_BLACK_SQUARE                                       0xfe
+#endif /* __MSDOS__ */
+
+/* Implementation */
+#ifdef RLHK_TUI_IMPLEMENTATION
+
+static int rlhk_tui_width;
+static int rlhk_tui_height;
+
+#if (defined(__unix__) || defined(__APPLE__)) && !defined(__DJGPP__)
+/* Here we're free to assume an int is 32-bits. */
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+
+struct termios rlhk_tui_termios_orig;
+
+/* The last written display. */
+static unsigned rlhk_tui_oldc[RLHK_TUI_MAX_HEIGHT][RLHK_TUI_MAX_WIDTH];
+static unsigned char rlhk_tui_olda[RLHK_TUI_MAX_HEIGHT][RLHK_TUI_MAX_WIDTH];
+
+/* Characters to be written on the next flush. */
+static unsigned rlhk_tui_bufc[RLHK_TUI_MAX_HEIGHT][RLHK_TUI_MAX_WIDTH];
+static unsigned char rlhk_tui_bufa[RLHK_TUI_MAX_HEIGHT][RLHK_TUI_MAX_WIDTH];
+
+static unsigned char *
+rlhk_tui_itoa(unsigned char *p, int v)
+{
+    int len = !v;
+    int i;
+    for (i = v; i; i /= 10)
+        len++;
+    for (i = len - 1; i >= 0; i--, v /= 10)
+        p[i] = v % 10 + '0';
+    return p + len;
+}
+
+RLHK_TUI_API
+int
+rlhk_tui_init(int width, int height)
+{
+    struct termios raw;
+    char init[] = {
+        "\x1b[2J"   /* Clear the screen. */
+        "\x1b[?25l" /* Hide the cursor. */
+    };
+    rlhk_tui_width = width;
+    rlhk_tui_height = height;
+    tcgetattr(STDIN_FILENO, &rlhk_tui_termios_orig);
+    memcpy(&raw, &rlhk_tui_termios_orig, sizeof(raw));
+    raw.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
+    raw.c_oflag &= ~OPOST;
+    raw.c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN);
+    raw.c_cflag &= ~(CSIZE|PARENB);
+    raw.c_cflag |= CS8;
+    tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+    return write(STDIN_FILENO, init, sizeof(init) - 1) == sizeof(init) - 1;
+}
+
+RLHK_TUI_API
+void
+rlhk_tui_release(void)
+{
+    char finish[24] =
+        "\x1b[?25h"  /* Restore cursor visibility. */
+        "\x1b[";     /* Move cursor just outside drawing region. */
+    unsigned char *f = (unsigned char *)finish + strlen(finish);
+    char *p = (char *)rlhk_tui_itoa(f, rlhk_tui_height);
+    strcpy(p, ";0H" "\x1b[0m\n");  /* Disable color/style. */
+    tcsetattr(STDIN_FILENO, TCSANOW, &rlhk_tui_termios_orig);
+    write(STDIN_FILENO, finish, sizeof(finish) - 1);
+    memset(rlhk_tui_oldc, 0, sizeof(rlhk_tui_oldc));
+}
+
+RLHK_TUI_API
+void
+rlhk_tui_putc(int x, int y, unsigned c, unsigned attr)
+{
+    rlhk_tui_bufc[y][x] = c;
+    rlhk_tui_bufa[y][x] = attr;
+}
+
+RLHK_TUI_API
+void
+rlhk_tui_flush(void)
+{
+    int x, y;
+    /* Estimated worst-case buffer. */
+    static unsigned char buf[RLHK_TUI_MAX_HEIGHT * RLHK_TUI_MAX_WIDTH *
+                             (5 + 11 + 3)];
+    unsigned char *p = buf;
+    unsigned last_a = -1u;
+    int cx = -1;
+    int cy = -1;
+    for (y = 0; y < rlhk_tui_height; y++) {
+        for (x = 0; x < rlhk_tui_width; x++) {
+            unsigned *oc = &rlhk_tui_oldc[y][x];
+            unsigned char *oa = &rlhk_tui_olda[y][x];
+            unsigned c = rlhk_tui_bufc[y][x];
+            unsigned a = rlhk_tui_bufa[y][x];
+            if (*oc != c || *oa != a) {
+                /* Move to location. */
+                if (x != cx || y != cy) {
+                    *p++ = 0x1b;
+                    *p++ = '[';
+                    p = rlhk_tui_itoa(p, y + 1);
+                    *p++ = ';';
+                    p = rlhk_tui_itoa(p, x + 1);
+                    *p++ = 'H';
+                    last_a = -1u;
+                }
+                /* Apply colors. */
+                if (a != last_a) {
+                    int fg = (a & 0x07) + 30;
+                    int bg = ((a >> 4) & 0x07) + (a & 0x80 ? 100 : 40);
+                    *p++ = 0x1b;
+                    *p++ = '[';
+                    if (a & 0x08) {
+                        *p++ = '1';
+                        *p++ = ';';
+                    }
+                    p = rlhk_tui_itoa(p, fg);
+                    *p++ = ';';
+                    p = rlhk_tui_itoa(p, bg);
+                    *p++ = 'm';
+                    last_a = a;
+                }
+                cx = x + 1;
+                cy = y;
+                *oc = c;
+                *oa = a;
+                do {
+                    *p++ = c;
+                    c >>= 8;
+                } while (c);
+            }
+        }
+    }
+    write(STDOUT_FILENO, buf, p - buf);
+}
+
+RLHK_TUI_API
+int
+rlhk_tui_getch(void)
+{
+    int r;
+    unsigned char c;
+    if ((r = read(STDIN_FILENO, &c, sizeof(c))) < 0) {
+        return r;
+    } else if (c == '\x1b') {
+        unsigned char code[2];
+        (void)read(STDIN_FILENO, code, sizeof(code));
+        return code[1] + 256;
+    } else {
+        return c;
+    }
+}
+
+RLHK_TUI_API
+void
+rlhk_tui_title(const char *title)
+{
+    char a = '\a';
+    write(STDIN_FILENO, "\x1b]2;", 4);
+    write(STDIN_FILENO, title, strlen(title));
+    write(STDIN_FILENO, &a, 1);
+}
+
+RLHK_TUI_API
+void
+rlhk_tui_size(int *width, int *height)
+{
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    *width = w.ws_col;
+    *height = w.ws_row;
+}
+
+#elif defined(_WIN32)
+#include <windows.h>
+#include <conio.h>
+
+static CHAR_INFO rlhk_tui_buf[RLHK_TUI_MAX_HEIGHT * RLHK_TUI_MAX_WIDTH];
+static HANDLE rlhk_tui_out;
+static HANDLE rlhk_tui_in;
+static DWORD rlhk_tui_mode_orig;
+
+RLHK_TUI_API
+int
+rlhk_tui_init(int width, int height)
+{
+    CONSOLE_CURSOR_INFO info = {100, FALSE};
+    SMALL_RECT window = {0, 0, 0, 0};
+    COORD size;
+    DWORD mode;
+    rlhk_tui_width = width;
+    rlhk_tui_height = height;
+    rlhk_tui_out = GetStdHandle(STD_OUTPUT_HANDLE);
+    rlhk_tui_in  = GetStdHandle(STD_INPUT_HANDLE);
+
+    /* Adjust console input and cursor. */
+    SetConsoleCursorInfo(rlhk_tui_out, &info);
+    GetConsoleMode(rlhk_tui_in, &rlhk_tui_mode_orig);
+    mode = rlhk_tui_mode_orig & ~ENABLE_PROCESSED_INPUT;
+    SetConsoleMode(rlhk_tui_in, mode);
+
+    /* Adjust window size. */
+    window.Bottom = height - 1;
+    window.Right = width - 1;
+    if (!SetConsoleWindowInfo(rlhk_tui_out, TRUE, &window))
+        return 0;
+
+    /* Adjust buffer size. */
+    size.X = width;
+    size.Y = height;
+    if (!SetConsoleScreenBufferSize(rlhk_tui_out, size))
+        return 0;
+
+    return 1;
+}
+
+RLHK_TUI_API
+void
+rlhk_tui_release(void)
+{
+    CONSOLE_CURSOR_INFO info = {100, TRUE};
+    COORD coord = {0, 0};
+    coord.Y = rlhk_tui_height;
+    SetConsoleCursorInfo(rlhk_tui_out, &info);
+    SetConsoleCursorPosition(rlhk_tui_out, coord);
+    SetConsoleMode(rlhk_tui_in, rlhk_tui_mode_orig);
+}
+
+RLHK_TUI_API
+void
+rlhk_tui_putc(int x, int y, unsigned c, unsigned attr)
+{
+    rlhk_tui_buf[y * rlhk_tui_width + x].Char.UnicodeChar = c;
+    rlhk_tui_buf[y * rlhk_tui_width + x].Attributes =
+        (attr & 0xaa) | ((attr >> 2) & 0x11) | ((attr << 2) & 0x44);
+}
+
+RLHK_TUI_API
+void
+rlhk_tui_flush(void)
+{
+    COORD origin = {0, 0};
+    SMALL_RECT area = {0, 0, 0, 0};
+    COORD size;
+    area.Right = rlhk_tui_width;
+    area.Bottom = rlhk_tui_height;
+    size.X = rlhk_tui_width;
+    size.Y = rlhk_tui_height;
+    WriteConsoleOutputW(rlhk_tui_out, rlhk_tui_buf, size, origin, &area);
+}
+
+RLHK_TUI_API
+int
+rlhk_tui_getch(void)
+{
+    int result = getch();
+    if (result != 0xE0 && result != 0x00) {
+        return result;
+    } else {
+        result = getch();
+        switch (result) {
+            case 72:
+                return RLHK_TUI_VK_U;
+            case 80:
+                return RLHK_TUI_VK_D;
+            case 75:
+                return RLHK_TUI_VK_L;
+            case 77:
+                return RLHK_TUI_VK_R;
+            case 71:
+                return RLHK_TUI_VK_UL;
+            case 73:
+                return RLHK_TUI_VK_UR;
+            case 79:
+                return RLHK_TUI_VK_DL;
+            case 81:
+                return RLHK_TUI_VK_DR;
+            default:
+                return result + 256;
+        }
+    }
+}
+
+RLHK_TUI_API
+void
+rlhk_tui_title(const char *title)
+{
+    SetConsoleTitle(title);
+}
+
+RLHK_TUI_API
+void
+rlhk_tui_size(int *width, int *height)
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    *width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    *height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+}
+
+#elif defined(__DJGPP__)
+#ifdef __STRICT_ANSI__
+#  undef __STRICT_ANSI__  /* Disable DJGPP annoyance. */
+#endif
+#include <conio.h>
+#include <sys/nearptr.h>
 
 static unsigned short rlhk_tui_buf[25][80];
 
